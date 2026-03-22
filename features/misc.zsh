143d71a6c62bb33b7ed4ef8de8f58c276802e679
Smith Alike <sojenir@gmail.com>
2026-03-22 14:49:19 +0800

    zoxide optimization

--- features/misc.zsh
+++ features/misc.zsh
@@ -1,1 +1,16 @@
-eval "$(zoxide init zsh)"
+# --- Zoxide ---
+ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/zsh"
+ZOXIDE_CACHE="$ZSH_CACHE_DIR/zoxide_init.zsh"
+ZOXIDE_BIN=$(whence -p zoxide)
+
+[[ -d "$ZSH_CACHE_DIR" ]] || mkdir -p "$ZSH_CACHE_DIR"
+
+# 仅在必要时生成缓存
+if [[ -n "$ZOXIDE_BIN" ]]; then
+    if [[ ! -f "$ZOXIDE_CACHE" || "$ZOXIDE_BIN" -nt "$ZOXIDE_CACHE" ]]; then
+        zoxide init zsh >| "$ZOXIDE_CACHE"
+    fi
+    source "$ZOXIDE_CACHE"
+else
+    return 1 2>/dev/null
+fi
