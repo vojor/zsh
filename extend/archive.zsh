85d89ea4b5fb41f2f4ca31f2fb58d20c07051fdf
Smith Alike <sojenir@gmail.com>
2026-03-25 11:26:59 +0800

    normal version to optimi version

--- extend/archive.zsh
+++ extend/archive.zsh
@@ -5,8 +5,14 @@ extract_logic() {
     [[ -f "$file" ]] || { print -P "%F{red}󰅚 %f File '$file' not found."; return 1 }
     [[ -d "$dest" ]] || mkdir -p "$dest"
 
-    print -P "%F{blue}󰛒 %f Extracting '$file' to '$dest'..."
-    bsdtar -xpf "$file" -C "$dest" && print -P "%F{244}[%D{%H:%M:%S}]%f %F{green}󰄬 %fExtracting Done."
+    print -P "%F{blue}󰛒 %f Extracting %B%F{cyan}'$file'%f%b to %B%F{yellow}'$dest'%f%b..."
+
+    if bsdtar -xpf "$file" -C "$dest"; then
+        print -P "%F{244}[%D{%H:%M:%S}]%f %F{green}󰄬 %f Extracting Done"
+    else
+        print -P "%F{red}󰅚 %f Error:During extraction process."
+        return 1
+    fi
 }
 
 pack_logic() {
@@ -14,15 +20,24 @@ pack_logic() {
     shift
     local count=$#
 
-    [[ -z "$target" || $count -eq 0 ]] && { print -P "%F{yellow}Usage:%f p <arch.ext> <files...>"; return 1 }
+    if [[ -z "$target" || $count -eq 0 ]]; then
+        print -P "%F{yellow}󰋖 Usage:%f p <filename.ext> <files...>"
+        return 1
+    fi
 
     if [[ -z "${target:e}" ]]; then
         target="${target}.tar.gz"
         print -P "%F{yellow} %BWarnin: No extension detected,using default set:%b%f %F{blue}%B$target%b%f"
     fi
 
-    print -P "%F{blue}󰿖 %f Packing %B$count%b item(s) into '$target'..."
-    bsdtar -acf "$target" "$@" && print -P "%F{244}[%D{%H:%M:%S}]%f %F{green}󰄬 %f Created."
+    print -P "%F{blue}󰿖 %f Packing %B%F{magenta}$count%f%b item(s) into %B%F{cyan}'$target'%f%b..."
+
+    if bsdtar -acf "$target" "$@"; then
+        print -P "%F{244}[%D{%H:%M:%S}]%f %F{green}󰄬 %f Created Packaging"
+    else
+        print -P "%F{red}󰅚 %f Packaging Failed"
+        return 1
+    fi
 }
 
 (( $+commands[x] )) || alias x='extract_logic'
@@ -31,13 +46,15 @@ alias extract='extract_logic'
 alias pack='pack_logic'
 
 if [[ -n "$compstate" || -n "$functions[compdef]" ]]; then
-    local extract_cmds="(x|extract|extract_logic)"
-    zstyle ":completion:*:*:${extract_cmds}:*:*" file-patterns \
-        '*.tar(|.*):archives *.zip:archives *.7z:archives *.rar:archives *.zst:archives'
-    zstyle ":completion:*:*:${extract_cmds}:*:*" group-name ''
-    zstyle ":completion:*:*:${extract_cmds}:*:*" format '%F{blue}== %d ==%f'
-    zstyle ":completion:*:*:${extract_cmds}:*:*" tag-order 'archives'
+    local extract_cmds="x|extract|extract_logic"
+    local pack_cmds="p|pack|pack_logic"
 
-    zstyle ":completion:*:*:(p|pack|pack_logic):*:*" file-patterns \
+    zstyle ":completion:*:*:($extract_cmds):*:*" file-patterns \
+        '*.tar(|.*):archives *.zip:archives *.7z:archives *.rar:archives *.zst:archives'
+    zstyle ":completion:*:*:($pack_cmds):*:*" file-patterns \
         '*(/):directories %p:all-files'
+
+    zstyle ":completion:*:*:($extract_cmds|$pack_cmds):*:*" group-name ''
+    zstyle ":completion:*:*:($extract_cmds|$pack_cmds):*:*" format '%F{blue}== %d ==%f'
+    zstyle ":completion:*:*:($extract_cmds|$pack_cmds):*:*" tag-order 'archives'
 fi
