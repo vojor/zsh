3092ae575e60e52643b13561b96ef426fe3255fb
Smith Alike <sojenir@gmail.com>
2026-03-26 00:22:51 +0800

    Update

--- plugins/antidote.zsh
+++ plugins/antidote.zsh
@@ -1,5 +1,5 @@
-ZSH_PLUGIN_FILE="$ZSH_CONFIG_DIR/plugins.txt"
-ZSH_BUNDLE_FILE="$ZSH_CACHE_DIR/plugins.zsh"
+ZSH_PLUGIN_FILE="$ZSH_CONFIG_DIR/zsh_plugins.txt"
+ZSH_BUNDLE_FILE="$ZSH_CACHE_DIR/zsh_plugins.zsh"
 
 # Load antidote
 if [[ -f "${XDG_DATA_HOME}/.antidote/antidote.zsh" ]]; then
@@ -13,8 +13,8 @@ fi
 antidote-update() {
     print -P "%F{blue}󰚰 %BUpdating bundle to install plugins...%b%f"
 
-    if [[ -z "$all_proxy" && -z "$http_proxy" && -z "$https_proxy" ]]; then
-        print -P "%F{244}[%D{%H:%M:%S}]%f %F{yellow} %BWarning:%b%f %F{242}No proxy detected. Skipping plugin bundle.%f"
+    if [[ -z "$all_proxy" ]] && [[ -z "$http_proxy" || -z "$https_proxy" ]]; then
+        print -P "%F{244}[%D{%H:%M:%S}]%f %F{yellow} %BWarning:%b%f %F{242}No valid proxy detected. Skipping bundle update.%f"
         return 1
     fi
 
@@ -39,10 +39,17 @@ antidote-update() {
     fi
 }
 
-# Startup load logic
-if [[ -f "$ZSH_BUNDLE_FILE" && "$ZSH_PLUGIN_FILE" -ot "$ZSH_BUNDLE_FILE" ]]; then
-    source "$ZSH_BUNDLE_FILE"
-else
+if [[ ! -f "$ZSH_BUNDLE_FILE" || "$ZSH_PLUGIN_FILE" -nt "$ZSH_BUNDLE_FILE" ]]; then
     print -P "%F{green}󱐥 %BInitializing or updating plugins...%b%f"
-    antidote-update
+
+    if antidote-update; then
+        :
+    elif [[ -f "$ZSH_BUNDLE_FILE" ]]; then
+        print -P "%F{242}󰔚 %BWarning:%b%f %F{242}Update failed, falling back to existing bundle.%f"
+        source "$ZSH_BUNDLE_FILE"
+    else
+        print -P "%F{red} %BError:%b%f %F{172}No plugins loaded.%f"
+    fi
+else
+    source "$ZSH_BUNDLE_FILE"
 fi
