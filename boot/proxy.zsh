cca88b7f288e5369378612c3650b79d22f13babf
Smith Alike <sojenir@gmail.com>
2026-03-29 06:52:58 +0800

    update proxy

--- boot/proxy.zsh
+++ boot/proxy.zsh
@@ -1,26 +1,22 @@
 proxy_on() {
     local proxy_host=""
-    local proxy_port="10810"
+    local proxy_port=""
 
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        proxy_host="127.0.0.1"
-    elif grep -qi "microsoft" /proc/version 2>/dev/null; then
+    if grep -qi "microsoft" /proc/version 2>/dev/null; then
+        proxy_port="10810"
         proxy_host=$(ip route show | grep default | awk '{print $3}')
     else
-        proxy_host=$(ip route show | grep default | awk '{print $3}')
-        [[ -z "$proxy_host" ]] && proxy_host="127.0.0.1"
+        proxy_port="10809"
+        proxy_host="127.0.0.1"
     fi
 
     : ${proxy_host:=127.0.0.1}
+    : ${proxy_port:=10809}
 
     local proxy_url="http://${proxy_host}:${proxy_port}"
 
-    export http_proxy="$proxy_url"
-    export https_proxy="$proxy_url"
-    export all_proxy="$proxy_url"
-    export HTTP_PROXY="$proxy_url"
-    export HTTPS_PROXY="$proxy_url"
-    export ALL_PROXY="$proxy_url"
+    export {http,https,all}_proxy="$proxy_url"
+    export {HTTP,HTTPS,ALL}_PROXY="$proxy_url"
 
     print -P "%F{244}[%D{%H:%M:%S}]%f %F{76}󱊟 %BProxy Success On:%b%f %F{32}${proxy_url}%f"
 }
