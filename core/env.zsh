1da38486a4367f5ca7347c0a9f22ccacdf656eda
Smith Alike <sojenir@gmail.com>
2026-03-29 22:31:42 +0800

    update

--- core/env.zsh
+++ core/env.zsh
@@ -1,10 +1,14 @@
 export LANG=zh_CN.UTF-8
 export LANGUAGE=zh_CN:en_US
 export LC_ALL=zh_CN.UTF-8
+export LC_CTYPE=zh_CN.UTF-8
 
 export EDITOR='nvim'
-export MANPAGER='nvim +Man! -'
+export VISUAL='nvim'
+export MANPAGER='nvim +Man!'
 
-export LESS='-R --mouse --wheel-lines=3'
+export LESS='-R --mouse --wheel-lines=3 -i -j4 -M -F'
 export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/ripgrep/.ripgreprc"
+
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
